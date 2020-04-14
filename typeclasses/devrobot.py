b02e1304ce29c61b31e6b04198db46a5ab5718b0
elixx <elixx@drastical.net>
2020-04-14 21:05:49 +0000

    Update typeclasses/devrobot.py

--- typeclasses/devrobot.py
+++ typeclasses/devrobot.py
@@ -4,7 +4,6 @@ import random
 from evennia import utils
 from evennia import default_cmds
 from evennia import CmdSet
-from evennia import DefaultScript
 
 class devRobot01(Character):
     def at_object_creation(self):
@@ -17,8 +16,7 @@ class devRobot01(Character):
         # find out).
         self.db.get_err_msg = "The robot beeps at you, angrily. That's not a good idea."
         # We don't want to add the command to the robot.. but users in the same room...
-        self.cmdset.add(DevRobotCmdSet, permanent=True)
-        self.scripts.add(ScriptPokeRobot)
+        self.cmdset.add_default(DevRobotCmdSet, permanent=True)
         
         self.db.max = 20
         if(self.db.quotes is None):
@@ -90,44 +88,11 @@ class CmdRobotPoke(default_cmds.MuxCommand):
             self.caller.location.msg_contents("%s pokes the robot." % (self.caller.name), exclude=self.caller)
             self.obj.doQuote()
         
-    
 class DevRobotCmdSet(CmdSet):
     key = "DevRobotCmdSet"
 
     def at_cmdset_creation(self):
         self.add(CmdRobotPoke())
-
-class ScriptPokeRobot(DefaultScript):
-    def at_script_creation(self):
-        "Called when script first created."
-        self.key = "poke_robot"
-        self.desc = "Script that manages poking the robot."
-        self.persistent = True
-
-    def at_start(self):
-        """
-        This is called once every server restart, so we want to add the
-        (memory-resident) cmdset to the object here. is_valid is automatically
-        checked so we don't need to worry about adding the script to an
-        open lid.
-        """
-        # All we do is add the cmdset for the closed state.
-        self.obj.cmdset.add("typeclasses.devrobot.DevRobotCmdSet")
-
-    def is_valid(self):
-        """
-        The script is only valid while the lid is closed.
-        self.obj is the red_button on which this script is defined.
-        """
-        #return not self.obj.db.lid_open
-        return True
-
-    def at_stop(self):
-        """
-        When the script stops we must make sure to clean up after us.
-
-        """
-        self.obj.cmdset.delete("typeclasses.devrobot.DevRobotCmdSet")
 
 
 
