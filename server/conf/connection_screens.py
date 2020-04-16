16d256fad19ae7595a3babeaa4d8b8f269646cdf
elixx <elixx@drastical.net>
2020-04-16 09:01:24 -0400

    guest

--- server/conf/connection_screens.py
+++ server/conf/connection_screens.py
@@ -25,15 +25,17 @@ from evennia import utils
 
 CONNECTION_SCREEN = """
 |b==============================================================|n
- Welcome to |g{}|n, version {}!
-
+ Welcome to |g{}|n !
+ {}
+ 
  If you have an existing account, connect to it by typing:
       |wconnect <username> <password>|n
  If you need to create an account, type (without the <>'s):
       |wcreate <username> <password>|n
+ You can also log in to just look around:
+      |wconnect guest|n
 
  If you have spaces in your username, enclose it in quotes.
  Enter |whelp|n for more info. |wlook|n will re-show this screen.
 |b==============================================================|n""".format(
-    settings.SERVERNAME, utils.get_evennia_version("short")
-)
+    settings.SERVERNAME, settings.GAME_SLOGAN)
