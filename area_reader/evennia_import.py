b25818b8861798decceb11508ab85ab64ec1c083
elixx <elixx@drastical.net>
2020-04-29 21:11:44 -0400

    WIP: item imports

--- area_reader/evennia_import.py
+++ area_reader/evennia_import.py
@@ -29,12 +29,12 @@ class AreaImporter(object):
     def __init__(self, filename):
         self.area = {}
         self.translate = {}
-        area_file = area_reader.RomAreaFile(filename)
-        area_file.load_sections()
-        self.areaname = area_file.area.name
+        self.area_file = area_reader.RomAreaFile(filename)
+        self.area_file.load_sections()
+        self.areaname = self.area_file.area.name
         if self.areaname == "":
-            self.areaname = area_file.area.original_filename
-        for i, v in area_file.area.rooms.items():
+            self.areaname = self.area_file.area.original_filename
+        for i, v in self.area_file.area.rooms.items():
             name = v.name
             desc = v.description
             exits = {}
@@ -45,6 +45,17 @@ class AreaImporter(object):
             self.area[v.vnum] = {'name': name,
                                  'desc': desc,
                                  'exits': exits}
+
+        self.objects = {}
+        for i in self.area_file.area.objects.keys():
+            o = self.area_file.area.objects[i]
+            vnum = o.vnum
+            aliases = str(o.name).split(' ')
+            name = o.short_desc
+            desc = o.description
+            ext = o.extra_descriptions
+            itype = o.item_type
+            self.objects[vnum] = {'name': name, 'desc': desc, 'ext': ext, 'type': itype, 'aliases': aliases}
 
     def spawnRooms(self):
         firstRoom = True
@@ -81,5 +92,32 @@ class AreaImporter(object):
                     print("Exit " + exitDir + " in " + str(evid) + " skipped - vloc " + str(exitData['dest']) + " not found.")
                     continue
 
+    def spawnTranslation(self):
+        self.object_location = {}
+        for r in self.area_file.area.resets:
+            if r.command == 'O':
+                try:
+                    self.object_location[r.arg1] = self.translate[r.arg3]
+                except Exception as e:
+                    print(str(e) + "arg1=" + str(r.arg1) + " arg3=" + str(r.arg3))
+
     def spawnItems(self):
-        pass
+        print("spawning items")
+        for vnum,ob in self.objects.items():
+            try:
+                evloc = "#" + str(self.object_location[vnum])
+            except Exception as e:
+                print("vnum not in translation: #" + str(vnum) + str(e))
+
+            try:
+                loc = search_object(evloc)[0]
+            except:
+                loc = None
+            newob = create_object(key=ob['name'], location=loc, home=loc, aliases=ob['aliases'],
+                                  attributes=[('desc', ob['desc']),
+                                              ('ext_desc', ob['ext']),
+                                              ('type', ob['type'])])
+            self.translate[vnum] = newob.id
+
+
+
