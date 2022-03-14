5e143b564bf23f2b709a00bb6f2c7f19e59cd231
elixx <elixx@drastical.net>
2022-03-14 19:29:28 -0400

    move gametime function to world utils

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -5,22 +5,16 @@ Rooms are simple containers that has no location of their own.
 
 """
 
-import datetime
 import re
 from django.conf import settings
 from evennia import DefaultRoom
-from evennia import gametime
 from evennia import search_object
 from evennia import search_channel
 from evennia.commands.cmdset import CmdSet
 from evennia.utils import utils
+from world.utils import get_time_and_season
 
 COMMAND_DEFAULT_CLASS = utils.class_from_module(settings.COMMAND_DEFAULT_CLASS)
-
-MONTHS_PER_YEAR = settings.MONTHS_PER_YEAR
-SEASONAL_BOUNDARIES = settings.SEASONAL_BOUNDARIES
-HOURS_PER_DAY = settings.HOURS_PER_DAY
-DAY_BOUNDARIES = settings.DAY_BOUNDARIES
 
 RE_FOOTER = re.compile(r"\{xIt is .*?claimed by .*?\.\{n", re.IGNORECASE)
 
@@ -55,42 +49,9 @@ class LegacyRoom(Room):
     def at_init(self):
         super().at_init()
 
-    def get_time_and_season(self):
-        """
-        Calculate the current time and season ids.
-        """
-        # get the current time as parts of year and parts of day.
-        # we assume a standard calendar here and use 24h format.
-        timestamp = gametime.gametime(absolute=True)
-        # note that fromtimestamp includes the effects of server time zone!
-        datestamp = datetime.datetime.fromtimestamp(timestamp)
-        season = float(datestamp.month) / MONTHS_PER_YEAR
-        timeslot = float(datestamp.hour) / HOURS_PER_DAY
-
-        # figure out which slots these represent
-        if SEASONAL_BOUNDARIES[0] <= season < SEASONAL_BOUNDARIES[1]:
-            curr_season = "spring"
-        elif SEASONAL_BOUNDARIES[1] <= season < SEASONAL_BOUNDARIES[2]:
-            curr_season = "summer"
-        elif SEASONAL_BOUNDARIES[2] <= season < 1.0 + SEASONAL_BOUNDARIES[0]:
-            curr_season = "autumn"
-        else:
-            curr_season = "winter"
-
-        if DAY_BOUNDARIES[0] <= timeslot < DAY_BOUNDARIES[1]:
-            curr_timeslot = "night"
-        elif DAY_BOUNDARIES[1] <= timeslot < DAY_BOUNDARIES[2]:
-            curr_timeslot = "morning"
-        elif DAY_BOUNDARIES[2] <= timeslot < DAY_BOUNDARIES[3]:
-            curr_timeslot = "afternoon"
-        else:
-            curr_timeslot = "evening"
-
-        return curr_season, curr_timeslot
-
     def update_description(self):
         update=False
-        (season, daytime) = self.get_time_and_season()
+        (season, daytime) = get_time_and_season()
         if season != self.ndb.last_season:
             self.ndb.last_season = season
             update=True
