bb27f1a0e47174221d6bfe09b087267b6c650309
elixx <elixx@drastical.net>
2022-03-18 18:12:44 -0400

    whats in a name?

--- world/utils.py
+++ world/utils.py
@@ -207,3 +207,37 @@ def rename_tag(old_tag_name, old_category, new_name, new_category=None):
     for obj in objs:
         obj.tags.add(new_name, category=new_category)
         obj.tags.remove(old_tag_name, category=old_category)
+
+def fixtags():
+    areas = { "pawmist": "twilight city of pawmist",
+              "erealms": "elven realms",
+              "shadval150": "kandahar shadow valley",
+              "sdearth": "south dearthwood",
+              "edearth": "east dearthwood",
+             "avalonch": "avalon",
+             "talonvle": "gilda and the dragon",
+             "takshrin": "shrine of tahkien",
+            "dawn": "city of dawn",
+             "tisland": "treasure island",
+            "amazon":"the amazon jungle",
+            "partbody": "body parts castle",
+            "north": "northern road",
+            'river': 'durgas river',
+            'island': 'island of illusion',
+            'east': 'eastern road',
+            'demise': 'death room',
+            'path': 'the hidden path',
+            'gstrong': 'goblin stronghold',
+            'plains': 'plains of the north',
+            'pyramid': 'the great pyramid',
+              'weaverei': 'the dreamweaver\'s path',
+            'marsh': 'generic old marsh',
+            'tree': 'yggdrasil',
+            'zoology': 'mudschool fieldtrip',
+            'dock': 'calinth docks',
+            'water': 'blizzard water nymphs',
+            'chessbrd': 'chessboard'}
+
+    for area in areas.keys():
+        for a in ['area','room']:
+            rename_tag(area, a, areas[area], a)
