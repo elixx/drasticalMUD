8685cfb7bbec76f8252965700f00e83c831076c9
elixx <elixx@drastical.net>
2022-04-14 07:10:48 -0400

    rando spawn

--- world/utils.py
+++ world/utils.py
@@ -217,7 +217,14 @@ def spawnJunk(TRASH_SPAWN_PERCENT=10, BUNDLE_SPAWN_PERCENT=5):
         loc = choice(results)
         create_object(key=trash(), typeclass="typeclasses.resources.Resource", home=loc, location=loc)
 
+    from world.items import PRO_AXE, REPAIR_KIT
+    from evennia.prototypes.spawner import spawn
     for n in range(0, int(results.count() * (BUNDLE_SPAWN_PERCENT / 100))):
         loc = choice(results)
-        create_object(key='resource bundle', typeclass="typeclasses.resources.Resource", home=loc, location=loc,
+        ob = choice(PRO_AXE, REPAIR_KIT, 0)
+        if ob == 0:
+            create_object(key='resource bundle', typeclass="typeclasses.resources.Resource", home=loc, location=loc,
                       attributes=[('resources', {'wood': randint(0, 10), 'stone': randint(0, 10)})])
+        else:
+            ob['location'] = loc
+            spawn(ob)
