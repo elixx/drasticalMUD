aba789c085a6a725ff7d5a84659c0c815ec772d4
elixx <elixx@drastical.net>
2025-08-16 13:14:03 +0000

    junk cleanup

--- world/ticker.py
+++ world/ticker.py
@@ -1,29 +1,32 @@
 from evennia.utils.search import search_tag
-from evennia.utils.search import search_tag
-from world.utils import spawnJunk
+#from world.utils import spawnJunk
 
 
 def ticker_5min():
-    log("-- start world.ticker.ticker_5min --")
+    #log("-- start world.ticker.ticker_5min --")
     #
     # # Increment growth rooms
-    log("growth")
+    #log("growth")
     growable = search_tag("growable",category="object")
     for obj in growable:
         if obj.db.age >= 0 and obj.db.planted:
             obj.grow()
 
+    # # Junk cleanup
+    objs = search_tag("random_spawn", category='object')
+    for obj in objs[:500]:
+        obj.delete()
 
-    log("-- finish world.ticker.ticker_5min --")
+    #log("-- finish world.ticker.ticker_5min --")
     pass
 
 
 def ticker_daily():
-    log("-- start world.ticker.daily --")
+    #log("-- start world.ticker.daily --")
     spawnJunk()
 
     from typeclasses.shop import Merchant
     merchants = Merchant.objects.all()
     for m in merchants:
         m.new_stock(new_items=6)
-    log("-- finish world.ticker.daily --")
+    #log("-- finish world.ticker.daily --")
